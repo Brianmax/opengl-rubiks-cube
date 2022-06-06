f7b5000239261430fd149d1b26026148288921fe
Brianmax <george.maxi@ucsp.edu.pe>
2022-06-06 10:52:23 -0500

    feat: camara y refactorizacion

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -8,7 +7,6 @@
 #include <vector>
 #include <array>
 #include "vertex.cpp"
-
 using namespace std;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
@@ -51,7 +49,9 @@ float lastFrame = 0.0f;
 glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
-int len = 27;
+// timing
+
+int len = 3;
 struct cubito{
     unsigned int VBO, VAO;
     cubito()
@@ -77,103 +77,53 @@ struct camada{
         }
     }
 };
-
 struct Cube
 {
     unsigned int shaderP;
+    vector<camada> camadasCube;
     Cube(unsigned int shaderProgram){
         shaderP = shaderProgram;
+        camadasCube.resize(3);
     }
-
-    camada adelante = camada();
-    camada medio = camada();
-    camada atras = camada();
-
-    glm::vec3 cubePositionsFront[9] = {
-            glm::vec3(-0.8f, -0.8f, 0.0f),
-            glm::vec3(-0.3f, -0.8f, 0.0f),
-            glm::vec3(0.2f, -0.8f, 0.0f),
-
-            glm::vec3(-0.8f, -0.3f, 0.0f),
-            glm::vec3(-0.3f, -0.3f, 0.0f),
-            glm::vec3(0.2f, -0.3f, 0.0f),
-
-            glm::vec3(-0.8f, 0.2f, 0.0f),
-            glm::vec3(-0.3f, 0.2f, 0.0f),
-            glm::vec3(0.2f, 0.2f, 0.0f)
-    };
-
-    glm::vec3 cubePositionsMedium[9] = {
-            glm::vec3( -0.8f,  -0.8f, -0.5f),
-            glm::vec3( -0.3f,  -0.8f, -0.5f),
-            glm::vec3( 0.2f,  -0.8f, -0.5f),
-
-            glm::vec3( -0.8f,  -0.3f, -0.5f),
-            glm::vec3( -0.3f,  -0.3f, -0.5f),
-            glm::vec3( 0.2f,  -0.3f, -0.5f),
-
-            glm::vec3( -0.8f,  0.2f, -0.5f),
-            glm::vec3( -0.3f,  0.2f, -0.5f),
-            glm::vec3( 0.2f,  0.2f, -0.5f)
-    };
-
-    glm::vec3 cubePositionsBack[9] = {
-            glm::vec3( -0.8f,  -0.8f, -1.0f),
-            glm::vec3( -0.3f,  -0.8f, -1.0f),
-            glm::vec3( 0.2f,  -0.8f, -1.0f),
-
-            glm::vec3( -0.8f,  -0.3f, -1.0f),
-            glm::vec3( -0.3f,  -0.3f, -1.0f),
-            glm::vec3( 0.2f,  -0.3f, -1.0f),
-
-            glm::vec3( -0.8f,  0.2f, -1.0f),
-            glm::vec3( -0.3f,  0.2f, -1.0f),
-            glm::vec3( 0.2f,  0.2f, -1.0f)
-    };
-
     vector<vector<glm::vec4>> cubeColorsFront;
     vector<vector<glm::vec4>> cubeColorsMedium;
     vector<vector<glm::vec4>> cubeColorsBack;
 
-    void drawFront()
+    void draw()
     {
         // Para todos los cubitos
-        glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        view       = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
         unsigned int viewLoc  = glGetUniformLocation(shaderP, "view");
         unsigned int projectionLoc = glGetUniformLocation(shaderP,"projection");
-
-        // pass them to the shaders (3 different ways)
-        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-        //view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
         glUseProgram(shaderP);
 
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
         glUniformMatrix4fv(projectionLoc,1, GL_FALSE, &projection[0][0]);
 
-        for(int i = 0; i < 9; i++)
+        for(int i = 0; i < 3; i++)
         {
-            glm::mat4 currCubitoModel = glm::mat4(1.0f);
-            currCubitoModel = glm::translate(currCubitoModel, cubePositionsFront[i]);
-            unsigned int currCubitoModelLoc = glGetUniformLocation(shaderP, "model");
-            unsigned int colLoc = glGetUniformLocation(shaderP, "ourColor");
+            for(int e = 0; e < 9; e++)
+            {
+                glm::mat4 currCubitoModel = glm::mat4(1.0f);
+                currCubitoModel = glm::translate(currCubitoModel, positions[i][e]);
+                unsigned int currCubitoModelLoc = glGetUniformLocation(shaderP, "model");
+                unsigned int colLoc = glGetUniformLocation(shaderP, "ourColor");
 
-            glUniformMatrix4fv(currCubitoModelLoc, 1, GL_FALSE, &currCubitoModel[0][0]);
-            glUniform4f(colLoc, 0.0f, 1.0f, 0.0f, 1.0f);
+                glUniformMatrix4fv(currCubitoModelLoc, 1, GL_FALSE, &currCubitoModel[0][0]);
+                glUniform4f(colLoc, 1.0f, 0.0f, 0.0f, 1.0f);
 
-            glBindVertexArray(adelante.arr[i].VAO);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-            cout << "y estamos aqui, rafaga" << endl;
+                glBindVertexArray(camadasCube[i].arr[e].VAO);
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
         }
     }
-
-
 };
-int main() {
+int main(){
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -190,8 +140,14 @@ int main() {
         return -1;
     }
     glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // 1. Build and compile our shader programs
 
@@ -253,104 +209,7 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-
-    // 3. Set up vertex data and configure vertex attributes
-
-    // Define three vertices with 3D positions
-    float vertices[] = {
-            -0.2f, -0.2f, 0.0f,
-            0.2f, -0.2f, 0.0f,
-            0.0f,  0.2f, 0.0f
-    };
     glEnable(GL_DEPTH_TEST);
-
-    //Multiply(cube_vertices, 108);
-    unsigned int indices[] = {
-            0, 1, 3, // first triangle
-            1, 2, 3  // second triangle
-    };
-    //Multiply(cube_vertices, 24);
-    // Generate vertex buffer object (VBO) and vertex array object (VAO)
-    unsigned int arrVBO[len];
-    unsigned int arrVAO[len];
-    unsigned int arrEBO[1];
-    glm::vec3 cubePositions[] = {
-            glm::vec3( -0.8f,  -0.8f, 0.0f),
-            glm::vec3( -0.3f,  -0.8f, 0.0f),
-            glm::vec3( 0.2f,  -0.8f, 0.0f),
-
-            glm::vec3( -0.8f,  -0.3f, 0.0f),
-            glm::vec3( -0.3f,  -0.3f, 0.0f),
-            glm::vec3( 0.2f,  -0.3f, 0.0f),
-
-            glm::vec3( -0.8f,  0.2f, 0.0f),
-            glm::vec3( -0.3f,  0.2f, 0.0f),
-            glm::vec3( 0.2f,  0.2f, 0.0f),
-            ////////////////////////////////////////////
-            glm::vec3( -0.8f,  -0.8f, -0.5f),
-            glm::vec3( -0.3f,  -0.8f, -0.5f),
-            glm::vec3( 0.2f,  -0.8f, -0.5f),
-
-            glm::vec3( -0.8f,  -0.3f, -0.5f),
-            glm::vec3( -0.3f,  -0.3f, -0.5f),
-            glm::vec3( 0.2f,  -0.3f, -0.5f),
-
-            glm::vec3( -0.8f,  0.2f, -0.5f),
-            glm::vec3( -0.3f,  0.2f, -0.5f),
-            glm::vec3( 0.2f,  0.2f, -0.5f),
-
-            //////////////////////////////////////////////
-
-            glm::vec3( -0.8f,  -0.8f, -1.0f),
-            glm::vec3( -0.3f,  -0.8f, -1.0f),
-            glm::vec3( 0.2f,  -0.8f, -1.0f),
-
-            glm::vec3( -0.8f,  -0.3f, -1.0f),
-            glm::vec3( -0.3f,  -0.3f, -1.0f),
-            glm::vec3( 0.2f,  -0.3f, -1.0f),
-
-            glm::vec3( -0.8f,  0.2f, -1.0f),
-            glm::vec3( -0.3f,  0.2f, -1.0f),
-            glm::vec3( 0.2f,  0.2f, -1.0f),
-    };
-    /*for(int i = 0; i < len; i++)
-    {
-        glGenBuffers(1, &arrVBO[i]);
-        glGenVertexArrays(1, &arrVAO[i]);
-
-        glBindVertexArray(arrVAO[i]);
-
-        glBindBuffer(GL_ARRAY_BUFFER, arrVBO[i]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-        //printVertices(vertices, 9);
-    }*/
-    /* unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    // Bind VAO, then bind VBO
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // Copy the vertex data into the buffer's memory
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Set attributes that describe how OpenGL should interpret the vertex data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-
-    // Unbind so other calls won't modify VBO and VAO
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0); */
-    glLineWidth(10);
-
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -361,42 +220,7 @@ int main() {
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
-        cube.drawFront();
-
-
-        /*glm::mat4 view          = glm::mat4(1.0f);
-        glm::mat4 projection    = glm::mat4(1.0f);
-        //model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        //view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
-        unsigned int viewLoc  = glGetUniformLocation(shaderProgram, "view");
-        unsigned int projectionLoc = glGetUniformLocation(shaderProgram,"projection");
-        // pass them to the shaders (3 different ways)
-        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        const float radius = 10.0f;
-
-        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-        glUniformMatrix4fv(projectionLoc,1, GL_FALSE, &projection[0][0]);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
-        /*glUseProgram(shaderProgram);
-        glBindVertexArray(arrVAO[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);*/
-
-        /*for(int i = 0; i < len; i++) {
-            glm::mat4 model         = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            //float angle = 20.0f * i;
-            //model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
-            glUseProgram(shaderProgram);
-            glBindVertexArray(arrVAO[i]);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-            //glDrawArrays(GL_LINE, 0, 36);
-        }*/
+        cube.draw();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
--- vertex.cpp
+++ vertex.cpp
@@ -1,6 +1,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <vector>
 float cube_vertices[] = {
         -0.2f, -0.2f, -0.2f,
         0.2f, -0.2f, -0.2f,
@@ -45,7 +46,7 @@ float cube_vertices[] = {
         -0.2f,  0.2f, -0.2f
 };
 
-glm::vec3 cubePositions[] = {
+std::vector<glm::vec3> cubePositionsFront = {
         glm::vec3(-0.8f, -0.8f, 0.0f),
         glm::vec3(-0.3f, -0.8f, 0.0f),
         glm::vec3(0.2f, -0.8f, 0.0f),
@@ -57,19 +58,21 @@ glm::vec3 cubePositions[] = {
         glm::vec3(-0.8f, 0.2f, 0.0f),
         glm::vec3(-0.3f, 0.2f, 0.0f),
         glm::vec3(0.2f, 0.2f, 0.0f),
+};
+std::vector<glm::vec3> cubePositionsMiddle = {
+        glm::vec3(-0.8f, -0.8f, -0.5f),
+        glm::vec3(-0.3f, -0.8f, -0.5f),
+        glm::vec3(0.2f, -0.8f, -0.5f),
 
-        glm::vec3( -0.8f,  -0.8f, -0.5f),
-        glm::vec3( -0.3f,  -0.8f, -0.5f),
-        glm::vec3( 0.2f,  -0.8f, -0.5f),
+        glm::vec3(-0.8f, -0.3f, -0.5f),
+        glm::vec3(-0.3f, -0.3f, -0.5f),
+        glm::vec3(0.2f, -0.3f, -0.5f),
 
-        glm::vec3( -0.8f,  -0.3f, -0.5f),
-        glm::vec3( -0.3f,  -0.3f, -0.5f),
-        glm::vec3( 0.2f,  -0.3f, -0.5f),
-
-        glm::vec3( -0.8f,  0.2f, -0.5f),
-        glm::vec3( -0.3f,  0.2f, -0.5f),
-        glm::vec3( 0.2f,  0.2f, -0.5f),
-
+        glm::vec3(-0.8f, 0.2f, -0.5f),
+        glm::vec3(-0.3f, 0.2f, -0.5f),
+        glm::vec3(0.2f, 0.2f, -0.5f),
+};
+std::vector<glm::vec3> cubePositionsBack {
         glm::vec3( -0.8f,  -0.8f, -1.0f),
         glm::vec3( -0.3f,  -0.8f, -1.0f),
         glm::vec3( 0.2f,  -0.8f, -1.0f),
@@ -82,3 +85,5 @@ glm::vec3 cubePositions[] = {
         glm::vec3( -0.3f,  0.2f, -1.0f),
         glm::vec3( 0.2f,  0.2f, -1.0f)
 };
+
+std::vector<std::vector<glm::vec3>> positions = {cubePositionsFront, cubePositionsMiddle, cubePositionsBack};
